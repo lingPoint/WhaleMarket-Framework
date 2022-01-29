9f2fdd4ec537451a5e616908b6915a161c737bdc
Zero <1759407994@qq.com>
2022-01-29 17:23:52 +0800

    remove

---
+++ src/user/user.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+
+static const char* tbnron = "|ID         |Name       |Contact    |Address    |Balance    |";
+static const char* fgxian = "+-----------+-----------+-----------+-----------+-----------+";
