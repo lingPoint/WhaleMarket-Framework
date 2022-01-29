29b7944ae9b77f777ed3bf8e8fd2679914920744
Zero <1759407994@qq.com>
2022-01-29 17:22:09 +0800

    remove

---
+++ src/menu/menu.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char* menuPrompts[MENU_NUM] = {"\
++==============+================+===============+========+\n\
+| 1.User Login | 2.User Sign Up | 3.Admin Login | 4.Exit |\n\
++==============+================+===============+========+\n"}
